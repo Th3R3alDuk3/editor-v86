d4f881340d85f10b7d17e589a098234effb51f10
Th3R3alDuk3 <60510941+Th3R3alDuk3@users.noreply.github.com>
2021-05-27 19:43:19 +0200

    code review

---
+++ public/js/tinycore_gui.js
@@ -0,0 +1,34 @@
+"use strict";
+
+/* 
+ * V86 EMULATOR
+ * https://github.com/Th3R3alDuk3/v86
+ * 
+ * Changes: 
+ * SerialAdapterXtermJS
+ * serial0_send_line
+ */
+
+var answer = confirm("Download image using an asynchronous connection?");
+
+var parrot = new V86Starter({        
+    wasm_path: "../wasm/v86.wasm",
+    memory_size: 128 * 1024 * 1024,    
+    vga_memory_size: 16 * 1024 * 1024,
+    bios: {
+        url: "../assets/bios/seabios.bin",
+        size: 128 * 1024,
+        async: false
+    },
+    vga_bios: {
+        url: "../assets/bios/vgabios.bin",
+        size: 35 * 1024,
+        async: false
+    },
+    hda: {
+        url: "../assets/images/tinycore_gui.img",
+        async: answer
+    },
+    screen_container : document.getElementById("parrot"),
+    autostart: true
+});
